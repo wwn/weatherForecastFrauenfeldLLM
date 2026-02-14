e972d04449bef1b5e2a394efc0c19653bb1ec649
wwn <wwn@mailbox.org>
2026-02-14 17:16:47 +0100

    rename

--- src/test/java/ch/nickl/weatherForecastFrauenfeldLLM/integration/WeatherMappingIntegrationTest.java
+++ src/test/java/ch/nickl/weatherForecastFrauenfeldLLM/integration/WeatherMappingIntegrationTest.java
@@ -1,4 +1,4 @@
-package ch.nickl.weatherForecastFrauenfeldLLM.foobar;
+package ch.nickl.weatherForecastFrauenfeldLLM.integration;
 
 import ch.nickl.weatherForecastFrauenfeldLLM.domain.model.WeatherSnapshotEntity;
 import ch.nickl.weatherForecastFrauenfeldLLM.interfaces.rest.dto.OpenMeteoWeatherSnapshotResponseDto;
